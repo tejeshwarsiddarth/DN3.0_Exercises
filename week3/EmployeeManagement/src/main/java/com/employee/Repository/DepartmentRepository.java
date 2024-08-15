764c0728facb5127d83b10c1d5556955893da553
tejeshwarsiddarth <117458671+tejeshwarsiddarth@users.noreply.github.com>
2024-08-15 23:29:29 +0530

    Delete week3/EmployeeManagement directory

--- week3/EmployeeManagement/src/main/java/com/employee/Repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.employee.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employee.Entity.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    
-}
